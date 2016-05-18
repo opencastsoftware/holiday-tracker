a58243e5dfb3024d99fe4c26296a52eced91cbd2
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-05-18 13:44:16 +0100

    working form button

--- app/controllers/HolidayController.scala
+++ app/controllers/HolidayController.scala
@@ -1,16 +1,35 @@
 package controllers
 
+
+import play.api.data.Forms._
+import play.api.data.Form
 import play.api.mvc._
-import views.html.main
+import views.html.{main, hellothere}
 import models.HolidayData
 
 class HolidayController extends Controller {
 
+  val form = Form(
+    tuple(
+      "firstname" -> text,
+      "lastname" -> text
+    )
+  )
+
   def index = Action {
-    Ok(main())
+    Ok(main(form))
   }
 
   val test = HolidayData
+
+  def submit = Action { implicit request =>
+    val (fname, lname) = form.bindFromRequest.get
+    Ok("Hi %s %s".format(fname, lname))
+  }
+
+  def sayHello = Action {
+    Ok(hellothere())
+  }
 
   def holidayRemaining(name: String): Int = {
     val personInfo: List[(String, Int)] = HolidayData.remainingHolidays.filter(_._1 == name)
