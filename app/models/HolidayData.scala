d4d289fa30970c4bc0c5bfee43464278f1d0a800
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-05-19 15:56:02 +0100

    reading data from plain csv file

--- app/models/HolidayData.scala
+++ app/models/HolidayData.scala
@@ -1,13 +1,24 @@
 package models
 
+import scala.io.Source
+
 
 object HolidayData {
 
-  val remainingHolidays = List(("Bob", 3), ("Nicole", 10))
 
   def holidayRemaining(name: String): Int = {
-    val personInfo: List[(String, Int)] = HolidayData.remainingHolidays.filter(_._1 == name)
+    val personInfo: List[(String, Int)] = HolidayData.parseHolidayCsv.filter(_._1 == name)
     personInfo.head._2
   }
 
+  def parseHolidayCsv: List[(String, Int)] = {
+
+    val src = Source.fromFile("app/models/holidayCsv.csv")
+    val iter: List[String] = src.getLines().toList
+
+    iter.map { row =>
+      val entry = row.split(",")
+      (entry(0), entry(1).toInt)
+    }
+  }
 }
--- test/models/HolidayDataSpec.scala
+++ test/models/HolidayDataSpec.scala
@@ -14,4 +14,23 @@ class HolidayDataSpec extends PlaySpec with OneAppPerSuite {
       }
     }
   }
+
+  "parseHolidayCsv" must {
+    "return all the data from the csv file" in {
+      HolidayData.parseHolidayCsv mustBe List(
+        ("Nicole",10),
+        ("Bob",3),
+        ("Darwin",12),
+        ("Elfriede",16),
+        ("Candace",19),
+        ("Estela",4),
+        ("Marty",6),
+        ("Bonnie",8),
+        ("Dewey",23),
+        ("Corrie",21),
+        ("Lora",15),
+        ("Brianne",3)
+      )
+    }
+  }
 }
